1039f660636eb3aa1405321b2e99340f389e6080
emmanoel-amunik <emmanoelamunik@hotmail.com>
2023-12-04 22:48:50 -0300

    first commit

---
+++ Python_Jogos/forca.py
@@ -0,0 +1,18 @@
+def play_hangman():
+    print("*" * 29)
+    print("Welcome to the Hangman game!")
+    print("*" * 29)
+
+    secret_word = "banana"
+    hanged = False
+    right = True
+
+    while not hanged and not right:
+        print("Playing...")
+
+    print("*" * 29)
+    print("End Game")
+
+
+if __name__ == "__main__":
+    play_hangman()
